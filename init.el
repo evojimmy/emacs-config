f0bd50b5553cf3298b99e9a514b173dad156b91c
evojimmy <evolution.jimmy@gmail.com>
2014-01-26 23:05:39 +0800

    remove mode I dont use ++ Haskell mode

--- init.el
+++ init.el
@@ -1,55 +1,57 @@
 ;; -*- mode: emacs-lisp -*-
 ;; Simple .emacs configuration
 
-;; ---------------------
-;; -- Global Settings --
-;; ---------------------
 (add-to-list 'load-path "~/.emacs.d")
 
-(require 'cl)
-(require 'linum)
-(require 'smooth-scrolling)
-(require 'compile)
+(require 'cl)                       ;; [TODO] common-lisp??
+(require 'smooth-scrolling)         ;; allow one-line scroll by keyboard
 
-(menu-bar-mode -1)
-(normal-erase-is-backspace-mode 1)
-(setq column-number-mode t)
+(menu-bar-mode -1)                  ;; hide menu bar
+(normal-erase-is-backspace-mode 1)  ;; ensure function of Backspace key
+(setq column-number-mode t)         ;; display line number on status bar
 (setq inhibit-startup-message t)
 
-;; ------------
-;; -- Macros --
-;; ------------
-(load "defuns-config.el")
-(fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
-(global-set-key "\M-=" 'align-equals)
-(global-set-key "\C-c;" 'comment-or-uncomment-region)
-(global-set-key "\M-n" 'next5)
-(global-set-key "\M-p" 'prev5)
-(global-set-key "\M-o" 'other-window)
-(global-set-key "\M-i" 'back-window)
-(global-set-key "\C-z" 'zap-to-char)
-(global-set-key "\C-h" 'backward-delete-char)
-(global-set-key "\M-d" 'delete-word)
-(global-set-key "\M-h" 'backward-delete-word)
-(global-set-key "\M-u" 'zap-to-char)
+(global-set-key [f11] 'fullscreen)
+(global-set-key (kbd "C-.") 'redo)
 
-;; ---------------------------
-;; -- JS Mode configuration --
-;; ---------------------------
-(load "js-config.el")
-(add-to-list 'load-path "~/.emacs.d/jade-mode") ;; github.com/brianc/jade-mode
-(require 'sws-mode)
-(require 'jade-mode)
-(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
-(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)  ;; don't prompt unsaved buffer when killing emacs
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
+(setq-default indent-tabs-mode nil) ;; don't use tab as default
+
+(defun untab-all ()
+  (untabify (point-min) (point-max))
+   nil ) ; did not write buffer to disk
+
+(defun add-write-contents-hooks-hook ()
+  (add-hook
+   'write-contents-hooks
+   'untab-all
+     nil  ; APPEND  unrelated, explicit default nil as optional :)
+     t )) ; LOCAL   non-nil => make hook local
+
+;; more modes: http://www.emacswiki.org/CategoryModes
+(add-hook 'emacs-lisp-mode-hook #'add-write-contents-hooks-hook)
+(add-hook 'c-mode-common-hook   #'add-write-contents-hooks-hook)
+(add-hook 'sh-mode-hook         #'add-write-contents-hooks-hook)
+(add-hook 'text-mode-hook       #'add-write-contents-hooks-hook)
+(add-hook 'sql-mode-hook        #'add-write-contents-hooks-hook)
+(add-hook 'css-mode-hook        #'add-write-contents-hooks-hook)
+(add-hook 'web-mode-hook        #'add-write-contents-hooks-hook)
+
+;;;;;;FONTS TEST;;;;;;
+;; 0 O || 1 l I || ` ' " || # * % ^ & @ ! || ( ) || [ ] || { } || < >
+;; __中文__
+;; ← → ↑ ↓ ⇒ £ € © ® ™ — … ¶ † ● ★ ◇ ◆ α β ° π ⊕ ⊗ “ ” « » 「 」 ✓ ♠ ♣ ♥ ♦ ♪ ☺
+(set-frame-font
+;; "DejaVu Sans Mono-13")
+;; "Monospace-13")
+;; "Liberation Mono-13") ;; symbols are a little small
+ "文泉驿等宽微米黑-13")
 
 
-(load "custom.el")
-(load "coffee-mode.el")
-(load "less-mode.el")
 
-
-
+;; web mode conf
 (load "web-mode.el")
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
@@ -66,13 +68,19 @@
 )
 (add-hook 'web-mode-hook  'web-mode-hook)
 
-
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
-
 
 (setq web-mode-style-padding 1)
 (setq web-mode-script-padding 1)
 (setq web-mode-block-padding 0)
 (setq web-mode-comment-style 2)
+
+
+;;;Haskell mode
+(load "haskell-mode/haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
